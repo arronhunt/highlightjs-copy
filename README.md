7309730a7ecde21b66690dc7a3648e7f7a06274e
Arron Hunt <arronjhunt@gmail.com>
2021-06-13 10:24:00 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# highlightjs-copy
+
+A simple, accessible highlightjs plugin to add a copy button to your codeblocks.
+
+![](./assets/preview.png)
+
+## Install
+
+```html
+<script src="path/to/highlightjs-copy.min.js">
+```
+
+```html
+<link rel="stylesheet" href="path/to/>
+```
+
+## Usage
+
+Basic usage
+
+```javascript
+hljs.addPlugin(CopyButtonPlugin());
+```
+
+With a callback
+
+```javascript
+hljs.addPlugin(
+  CopyButtonPlugin(function (el, text) {
+    console.log("Copied to clipboard", text);
+  })
+);
+```
+
+## Customization
+
+| CSS selector           | Details                                                                                                                   |
+| ---------------------- | ------------------------------------------------------------------------------------------------------------------------- |
+| `.hljs-copy-wrapper`   | Applied to the parent `<pre>` element that wraps the .hljs code.                                                          |
+| `.hljs-copy-button`    | The copy button itself.                                                                                                   |
+| `[data-copied='true']` | This data attribute is applied to the copy button and is set to `true` for two seconds when the copy action is performed. |
+| `.hljs-copy-alert`     | A visually hidden status element that announces the copy confirmation to screen readers.                                  |
---
+++ index.js
@@ -0,0 +1,59 @@
+/**
+ *  @file highlight-copy.js
+ *  @author Arron Hunt <arron@gretel.ai>
+ *  @copyright Copyright 2021. All rights reserved.
+ */
+
+/**
+ * Adds a copy button to highlightjs code blocks
+ * @param {copyCallback} [callback] - Called after the copy event has occured
+ * @returns {Object} hljs plugin object.
+ */
+function CopyButtonPlugin(callback) {
+  return {
+    "after:highlightElement"({ el, text }) {
+      // Create the copy button and append it to the codeblock
+      let button = Object.assign(document.createElement("button"), {
+        innerHTML: "Copy",
+        className: "hljs-copy-button",
+      });
+      button.dataset.copied = false;
+      el.parentElement.classList.add("hljs-copy-wrapper");
+      el.parentElement.appendChild(button);
+
+      button.onclick = function () {
+        if (!navigator.clipboard) return;
+
+        navigator.clipboard
+          .writeText(text)
+          .then(function () {
+            button.innerHTML = "Copied!";
+            button.dataset.copied = true;
+
+            let alert = Object.assign(document.createElement("div"), {
+              role: "status",
+              className: "hljs-copy-alert",
+              innerHTML: "Copied to clipboard",
+            });
+            el.parentElement.appendChild(alert);
+
+            setTimeout(() => {
+              button.innerHTML = "Copy";
+              button.dataset.copied = false;
+              el.parentElement.removeChild(alert);
+              alert = null;
+            }, 2000);
+          })
+          .then(function () {
+            if (typeof callback === "function") return callback(el, text);
+          });
+      };
+    },
+  };
+}
+
+/**
+ * @callback copyCallback
+ * @param {Object} el - The code block element that was copied from.
+ * @param {string} text - The raw text copied to the clipboard.
+ */
