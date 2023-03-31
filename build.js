1ac80ad6f77d1b7b42b6ffdf37f75ea510181c12
Arron Hunt <arronjhunt@gmail.com>
2023-03-31 12:22:48 -0700

    modules support

--- build.js
+++ build.js
@@ -1,30 +1,51 @@
 const esbuild = require("esbuild");
-// Build with esbuild for both commonjs and global
-esbuild
-  .build({
-    entryPoints: ["./index.js"],
-    bundle: true,
-    outfile: "dist/highlightjs-copy.js",
-    format: "iife",
-    globalName: "HighlightCopy",
-    minify: true,
-    sourcemap: true,
-    target: ["es2015"],
-    define: {
-      "process.env.NODE_ENV": '"production"',
-    },
-  })
-  .catch(() => process.exit(1));
 
-// Build with esbuild for esm
-esbuild.build({
-  entryPoints: ["./index.js"],
-  bundle: true,
-  outfile: "dist/highlightjs-copy.esm.js",
-  minify: true,
-  sourcemap: true,
-  target: ["var"],
-  define: {
-    "process.env.NODE_ENV": '"production"',
-  },
+function createConfig(
+  entryPoints,
+  outfile,
+  format,
+  minify = false,
+  loader = {}
+) {
+  return {
+    entryPoints,
+    outfile,
+    bundle: true,
+    minify,
+    target: "es2015",
+    platform: "neutral",
+    format,
+    loader,
+  };
+}
+
+const configs = [
+  // JavaScript
+  createConfig(["./index.js"], "dist/highlightjs-copy.js", "iife"),
+  createConfig(["./index.js"], "dist/highlightjs-copy.min.js", "iife", true),
+  createConfig(["./index.js"], "dist/highlightjs-copy.cjs.js", "cjs"),
+  createConfig(["./index.js"], "dist/highlightjs-copy.cjs.min.js", "cjs", true),
+  createConfig(["./index.js"], "dist/highlightjs-copy.esm.js", "esm"),
+  createConfig(["./index.js"], "dist/highlightjs-copy.esm.min.js", "esm", true),
+
+  // CSS
+  createConfig(
+    ["./styles/highlightjs-copy.css"],
+    "dist/highlightjs-copy.css",
+    "cjs",
+    false,
+    { ".css": "css" }
+  ),
+  createConfig(
+    ["./styles/highlightjs-copy.css"],
+    "dist/highlightjs-copy.min.css",
+    "cjs",
+    true,
+    { ".css": "css" }
+  ),
+];
+
+Promise.all(configs.map((config) => esbuild.build(config))).catch((e) => {
+  console.error(e);
+  process.exit(1);
 });
