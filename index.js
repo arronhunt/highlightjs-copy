076319d68bc92aa3d917ed059d54395aebb175ca
foxfire52 <foxfire52@virgilio.it>
2024-10-26 22:04:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -59,6 +59,25 @@ class CopyButtonPlugin {
       window.getComputedStyle(el).padding
     );
 
+    let fallback_clipboard = ((text)=>{
+        var textBox = document.createElement("textarea");
+        textBox.value = text;
+        textBox.style.top = "0";
+        textBox.style.left = "0";
+        textBox.style.position = "fixed";
+        document.body.appendChild(textBox);
+        textBox.focus();
+        textBox.select();
+        try {
+            var success = document.execCommand('copy');
+            var msg = success ? 'succeeded' : 'failed';
+            console.log('Clipboard Fallback: Copying text command ' + msg);
+        } catch (e) {
+        console.error('Clipboard Fallback: Unable to copy', e);
+        }
+        document.body.removeChild(textBox);
+    });
+
     button.onclick = async () => {
       let newText = text;
       if (hook && typeof hook === "function") {
